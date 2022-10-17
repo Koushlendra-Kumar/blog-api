509da6e8c34fe3d4b01807e72fabdd3f2c4bbefe
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-10-18 04:30:09 +0530

    Create author schema

---
+++ models/Author.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const AuthorSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    timestamp: { type: String, required: true },
+    blogs: { type: mongoose.Schema.Types.ObjectId }
+})
+
+module.exports = mongoose.model('Author', AuthorSchema);
