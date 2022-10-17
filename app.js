c6fb6231c3aab38bc70f44a3a1fac0b82c867c74
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-10-17 05:47:12 +0530

    Add default database uri

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 //Database connection setup
-const mongodb_uri = process.env.MONGODB_URI || '';
+const mongodb_uri = process.env.MONGODB_URI || 'mongodb+srv://portfolioblogdefault:defaultblogPortfolio@portfolio-blog-default.zorslld.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to the database.'))
   .catch(err => console.log('Error occurred while connecting to the database.', err))
