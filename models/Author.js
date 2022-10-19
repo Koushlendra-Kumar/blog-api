e32bbaf6ecd57d3f5b7ba16b5167b972244eab5b
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-10-19 07:02:37 +0530

    Add blog ref to author schema

--- models/Author.js
+++ models/Author.js
@@ -5,7 +5,7 @@ const AuthorSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     timestamp: { type: String, required: true },
-    blogs: { type: mongoose.Schema.Types.ObjectId }
+    blogs: { type: mongoose.Schema.Types.ObjectId, ref: 'Blog' }
 })
 
 module.exports = mongoose.model('Author', AuthorSchema);
