db00b5c2edd3abe423603c9220106f57f2698fd5
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-10-31 10:45:55 +0530

    Create controllers for blogs

---
+++ controllers/blogController.js
@@ -0,0 +1,15 @@
+exports.get_blogs = (req, res, next) => {
+    res.status(200).json({msg: 'Not implemented yet!'})
+}
+
+exports.post_blog = (req, res, next) => {
+    res.status(200).json({msg: 'Not implemented yet!'})
+}
+
+exports.delete_blog = (req, res, next) => {
+    res.status(200).json({msg: 'Not implemented yet!'})
+}
+
+exports.update_blog = (req, res, next) => {
+    res.status(200).json({msg: 'Not implemented yet!'})
+}
