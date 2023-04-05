baffce76e5680d3f6bb2290ca9f644ff44c1bdf4
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-04-05 09:03:48 +0000

    Console log post req body

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,8 +1,11 @@
+const BlogModel = require('../models/Blog');
+
 exports.get_blogs = (req, res, next) => {
     res.status(200).json({msg: 'Not implemented yet!'})
 }
 
 exports.post_blog = (req, res, next) => {
+    console.log(req.body);
     res.status(200).json({msg: 'Not implemented yet!'})
 }
 
