aa6d29d8e013fb9ebb7d81b9dcb94d0f16b10563
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-04-06 11:08:52 +0530

    :fire: Remove some required fields

--- models/Blog.js
+++ models/Blog.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const BlogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true },
-    comments: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }
+    // author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true },
+    // comments: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }
 })
 
 module.exports = mongoose.model('Blog', BlogSchema);
