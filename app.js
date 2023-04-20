1aeede1e6ab7b000d4967a58dc16cf55fc23388f
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-04-20 16:02:06 +0530

    :art: Change db route

--- app.js
+++ app.js
@@ -3,23 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
 const cors = require('cors');
 
+const db = require('./services/db');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-
-//Database connection setup
-const mongodb_uri = process.env.MONGODB_URI || 'mongodb+srv://portfolioblogdefault:defaultblogPortfolio@portfolio-blog-default.zorslld.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to the database.'))
-  .catch(err => console.log('Error occurred while connecting to the database.', err))
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(cors());
 app.use(logger('dev'));
