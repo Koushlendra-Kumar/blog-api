e5aa20dc29c3a496737b2e75b0440ec9873fd3b3
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-04-22 17:21:29 +0530

    :construction: Create function to reset password

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,3 +5,7 @@ exports.post_login = (req, res, next) => {
 exports.post_singup = (req, res, next) => {
     res.status(200).json({msg: 'Not implemented yet!'})
 }
+
+exports.post_forgot_password = (req, res, next) => {
+    res.json({msg: 'Not implemented yet!'})
+}
