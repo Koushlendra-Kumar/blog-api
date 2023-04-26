229d8b62e2d5b7f91ac00f477abb3d5bcfff3f87
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-04-26 14:38:50 +0530

    :passport_control: Add signup and login feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,38 @@
-exports.post_login = (req, res, next) => {
-    res.status(200).json({msg: 'Not implemented yet!'})
-}
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+exports.post_login = async (req, res, next) => {
+    passport.authenticate(
+      'login',
+      async (err, user, info) => {
+        try {
+          if (err || !user) {
+            const error = new Error('An error occurred.');
+
+            return next(error);
+          }
+
+          req.login(
+            user,
+            { session: false },
+            async (error) => {
+              if (error) return next(error);
+
+              const body = { _id: user._id, email: user.email };
+              const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+              return res.json({ token });
+            }
+          );
+        } catch (error) {
+          return next(error);
+        }
+      }
+    )(req, res, next);
+  }
 
 exports.post_singup = (req, res, next) => {
-    res.status(200).json({msg: 'Not implemented yet!'})
+    res.status(200).json({msg: 'Signed up successfully', user: req.user})
 }
 
 exports.post_forgot_password = (req, res, next) => {
